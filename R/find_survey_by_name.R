e608367c63896af976e5f24c93dbcc26332de19e
Sam Firke <samuel.firke@gmail.com>
2017-03-30 20:28:07 -0400

    add function to find_survey_by_name
    since the number could change and isn't memorable

---
+++ R/find_survey_by_name.R
@@ -0,0 +1,22 @@
+#' @title Fetch a survey by name.
+#'
+#' @description
+#' Supply the name of your survey and a survey_list and get back the \code{sm_survey} object.
+#'
+#' @param s_title the exact name of the survey to retrieve.
+#' @param survey_list the survey list to retrieve from
+#'
+#' @return the desired sm_survey object.
+#' @export
+#'
+#' @examples
+#' # not run:
+#' # s <- survey_list(per_page = 200)
+#' # my_survey <- find_survey_by_name("survey for testing", s)
+
+find_survey_by_name <- function(s_title, survey_list){
+  titles <- unlist(
+    lapply(survey_list, function(x) x$title)
+  )
+  survey_list[[which(titles == s_title)]]
+}
