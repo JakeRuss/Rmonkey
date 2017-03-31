e6d1c23b9929bbdef04121a5ed7a1bb36f8af0ff
Sam Firke <samuel.firke@gmail.com>
2017-03-30 20:27:45 -0400

    add background file that also imports %>%

---
+++ R/Rmonkey.R
@@ -0,0 +1,11 @@
+#' janitor
+#' 
+#' Rmonkey provides access to the SurveyMonkey API from R.
+#' #' 
+#' @docType package
+#' @name Rmonkey
+#' @importFrom dplyr %>%
+#' @keywords internal
+NULL
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
